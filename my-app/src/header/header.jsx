f8e838a4ebe25d1850cced68b7ff4a94817b09ae
Vlad Zh <deathgan0880@gmail.com>
2023-03-23 20:17:32 +0300

    Card commit

--- my-app/src/header/header.jsx
+++ my-app/src/header/header.jsx
@@ -1,14 +1,8 @@
 import styles from './style.module.css'
 
 
-const Card = ({imageUrl, price, title}) => {
-    return (
-        <div className={styles.cardContainer}>
-            <img src={imageUrl} alt="" />
-            <title>{title}</title>
-            <p>{price}</p>
-        </div> 
-    )
+const Card = () => {
+    
 }
 
 export default Card;

a23ad2807a92115fc916bbe28980e40d779c4032
Vare4ka <deathgan0880@gmail.com>
2023-03-24 15:46:23 +0300

    Card

--- my-app/src/header/Card.js
+++ my-app/src/header/Card.js
@@ -1,8 +1,16 @@
 import styles from './style.module.css'
 
 
-const Card = () => {
-    
+const Card = (imageUrl, title, price) => {
+    return(
+         <div className = {styles.cardContainer}>
+             <img src = {imageUrl} alt = ""/>
+             <title>{title}</title>
+              <p>{price}</p>
+
+         </div>
+
+    )  
 }
 
 export default Card;

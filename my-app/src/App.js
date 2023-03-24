6e1e74a57972135882f900211bcfb30ec9af2e41
Vare4ka <deathgan0880@gmail.com>
2023-03-24 14:42:01 +0300

    Card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-
-const App = () => {
+import Card from "./header/Card";
+  const App = () => {
 
   return (
     <>
-
+      <Card imageUrl="https://ae01.alicdn.com/kf/Hbcd23d6e97da45ca94ca4dd22dc29627z/VANS-Old-Skool-classic-sneakers-skateboards-unisex-36-44.jpg" title="Vans" price="40" /> 
     </>
   )
 }

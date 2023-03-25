372787b7f4aa30aeceb86fa9e0b064bd79cc833c
Vlad Zh <deathgan0880@gmail.com>
2023-03-25 22:54:15 +0300

    line commit

--- my-app/src/components/Card/Card.js
+++ my-app/src/components/Card/Card.js
@@ -4,10 +4,9 @@ import styles from './style.module.css'
 const Card = ({imageUrl, title, price}) => {
     return(
       <div className={styles.cardContainer}>
-       <img src={imageUrl} alt="" />
-       <p className={styles.titleCard}>{title}</p>
-       <p>{title}</p> 
-       <p>{price}$</p>
+        <img src={imageUrl} alt="" />
+        <p className={styles.titleCard}>{title}</p>
+        <p>{price}$</p>
      </div>
     )
 }

b6ba756a083604f3c15a063ec6aa52ce06366c5f
Vare4ka <deathgan0880@gmail.com>
2023-03-25 13:06:57 +0300

    Card

--- my-app/src/app/App.js
+++ my-app/src/app/App.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD:my-app/src/App.js
-import Card from "./header/Card";
-  const App = () => {
 
-  return (
-    <>
-      <Card imageUrl="https://ae01.alicdn.com/kf/Hbcd23d6e97da45ca94ca4dd22dc29627z/VANS-Old-Skool-classic-sneakers-skateboards-unisex-36-44.jpg" title="Vans" price="40" /> 
-=======
 import Home from "../pages/Home";
 
 const App = () => {
@@ -13,7 +6,7 @@ const App = () => {
   return (
     <>
       <Home />
->>>>>>> d164714d4c44dbf681fd58dcb92ae25d37293d16:my-app/src/app/App.js
+
     </>
   )
 }

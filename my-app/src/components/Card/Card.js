9313fed92c85e07c5c22f59ae48c2712df6723e8
Vlad Zh <deathgan0880@gmail.com>
2023-03-25 22:35:58 +0300

    new commit

--- my-app/src/components/Card/Card.js
+++ my-app/src/components/Card/Card.js
@@ -5,12 +5,8 @@ const Card = ({imageUrl, title, price}) => {
     return(
       <div className={styles.cardContainer}>
        <img src={imageUrl} alt="" />
-<<<<<<< HEAD
        <p className={styles.titleCard}>{title}</p>
-=======
        <p>{title}</p> 
-       
->>>>>>> 0cbe9a373b7cc4ebe93bf8c39f29fe3b5c06ec0f
        <p>{price}$</p>
      </div>
     )

64fe52b288b941da5ec4a83c091e8a3cb703d726
Vlad Zh <deathgan0880@gmail.com>
2023-03-30 13:16:38 +0300

    new

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -5,9 +5,13 @@ import Card from "../../components/Card/Card"
 import axios from "axios"
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { useRef } from "react"
 
 const Home = () => {
     const [state, setState] = useState()
+
+    const catalog = useRef(null)
+    const footer = useRef(null)
 
     async function Response() {
        const { data } = await axios.get("/products.json")
@@ -22,13 +26,15 @@ const Home = () => {
                     <img src="img/logoWeb.png" alt="" />
                     <div className={styles.btnsContainer}>
                         <ul>
-                            <li className={styles.nav_btn}>Catalog</li>
-                            <li className={styles.nav_btn}>About us</li>
+                            <li className={styles.nav_btn} onClick={() => catalog.current.scrollIntoView({block: "center"})}>Catalog</li>
+                            <li className={styles.nav_btn} onClick={() => footer.current.scrollIntoView({block: "center"})}>About us</li>
                             <li className={styles.accountAndCartContainer}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                                </svg>
+                                <Link to='/account'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                    </svg>
+                                </Link>
                                 <Link to="/cart">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
                                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
@@ -48,13 +54,13 @@ const Home = () => {
                 </p>
                 <button>Buy now</button>
             </header>
-            <div className={styles.productsContainer}>
+            <div className={styles.productsContainer} ref={catalog}>
                 {state ? state.map((item) => {
                 return <Card key={item.id} imageUrl={item.imageUrl} title={item.title} price={item.price} id={item.id} />   
                 }) : null}
             </div>
             
-            <footer>
+            <footer ref={footer}>
                 <div className={styles.contacts} >
                     <h2 >Our contacts</h2>
                          <p> +375290000000</p>
